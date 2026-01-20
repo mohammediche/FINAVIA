d4422be0da085410767e4915bb851b10332cec09
mohammediche <mohammediche@gmail.com>
2026-01-20 16:44:22 +0100

    fixed type issue

--- src/app/api/resource/route.ts
+++ src/app/api/resource/route.ts
@@ -20,7 +20,7 @@ export async function POST(req: Request) {
                 {
                     content: fileBuffer.toString('base64'),
                     name: 'guide-finavia.pdf',
-                    type: 'application/pdf'
+
                 }
             ];
         }
